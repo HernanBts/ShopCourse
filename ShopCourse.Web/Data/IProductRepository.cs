b45535b83729f3173e0310e3bf2ee4ea162d13f7
Hernán Bs <bort05@hotmail.com>
2019-03-15 06:49:57 -0300

    Add Images upload Add ProductWithUsers in API

--- ShopCourse.Web/Data/IProductRepository.cs
+++ ShopCourse.Web/Data/IProductRepository.cs
@@ -1,7 +1,10 @@
 ﻿namespace ShopCourse.Web.Data
 {
     using ShopCourse.Web.Data.Entities;
+    using System.Linq;
+
     public interface IProductRepository : IGenericRepository<Product>
     {
+        IQueryable GetAllWithUsers();
     }
 }

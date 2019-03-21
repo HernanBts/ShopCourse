eb5b837423976a82908165b48798fe8b439ddc71
Hernán Bs <bort05@hotmail.com>
2019-03-21 09:13:44 -0300

    Managment accounts

--- ShopCourse.Web/Helpers/UserHelper.cs
+++ ShopCourse.Web/Helpers/UserHelper.cs
@@ -1,8 +1,11 @@
 ﻿namespace ShopCourse.Web.Helpers
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Data.Entities;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
     using ShopCourse.Web.Models;
 
     public class UserHelper : IUserHelper
@@ -106,6 +109,25 @@
         public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
         {
             return await this.userManager.ResetPasswordAsync(user, token, password);
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            return await this.userManager.Users
+                .Include(u => u.City)
+                .OrderBy(u => u.FirstName)
+                .ThenBy(u => u.LastName)
+                .ToListAsync();
+        }
+
+        public async Task RemoveUserFromRoleAsync(User user, string roleName)
+        {
+            await this.userManager.RemoveFromRoleAsync(user, roleName);
+        }
+
+        public async Task DeleteUserAsync(User user)
+        {
+            await this.userManager.DeleteAsync(user);
         }
 
     }

a03e51b0e33eb18eb20d3e9f25249418949f6e53
HernanBts <bort05@hotmail.com>
2019-04-27 09:33:41 -0300

    add login with API in Classic iOS

--- ShopCourse.UIClassic.iOS/ViewController.cs
+++ ShopCourse.UIClassic.iOS/ViewController.cs
@@ -1,4 +1,6 @@
 ﻿using Foundation;
+using ShopCourse.Common.Models;
+using ShopCourse.Common.Services;
 using System;
 using UIKit;
 
@@ -6,6 +8,7 @@ namespace ShopCourse.UIClassic.iOS
 {
     public partial class ViewController : UIViewController
     {
+        private ApiService apiService;
         public ViewController (IntPtr handle) : base (handle)
         {
         }
@@ -13,6 +16,7 @@ namespace ShopCourse.UIClassic.iOS
         public override void ViewDidLoad ()
         {
             base.ViewDidLoad ();
+            this.apiService = new ApiService();
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
@@ -21,5 +25,62 @@ namespace ShopCourse.UIClassic.iOS
             base.DidReceiveMemoryWarning ();
             // Release any cached data, images, etc that aren't in use.
         }
+
+        partial void LoginBtn_TouchUpInside(UIButton sender)
+        {
+            if (string.IsNullOrEmpty(this.emailField.Text))
+            {
+                var alert = UIAlertController.Create("Error", "You must enter an email.", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+                this.PresentViewController(alert, true, null);
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.pwField.Text))
+            {
+                var alert = UIAlertController.Create("Error", "You must enter a password.", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+                this.PresentViewController(alert, true, null);
+                return;
+            }
+
+            //var ok = UIAlertController.Create("Ok", "Fuck yeah!", UIAlertControllerStyle.Alert);
+            //ok.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+            //this.PresentViewController(ok, true, null);
+
+            this.DoLogin();
+        }
+
+        private async void DoLogin()
+        {
+            this.activityIndicator.StartAnimating();
+            var request = new TokenRequest
+            {
+                Username = this.emailField.Text,
+                Password = this.pwField.Text
+            };
+
+            var response = await this.apiService.GetTokenAsync(
+                "https://shopbts.azurewebsites.net",
+                "/Account",
+                "/CreateToken",
+                request);
+
+            if (!response.IsSuccess)
+            {
+                this.activityIndicator.StopAnimating();
+                var alert = UIAlertController.Create("Error", "User or password incorrect.", UIAlertControllerStyle.Alert);
+                alert.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+                this.PresentViewController(alert, true, null);
+                return;
+            }
+
+            var token = (TokenResponse)response.Result;
+            this.activityIndicator.StopAnimating();
+            var ok = UIAlertController.Create("Ok", "Fuck yeah!", UIAlertControllerStyle.Alert);
+            ok.AddAction(UIAlertAction.Create("Accept", UIAlertActionStyle.Default, null));
+            this.PresentViewController(ok, true, null);
+        }
+
     }
 }
--- ShopCourse.UIClassic.iOS/ViewController.designer.cs
+++ ShopCourse.UIClassic.iOS/ViewController.designer.cs
@@ -14,8 +14,47 @@ namespace ShopCourse.UIClassic.iOS
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIActivityIndicatorView activityIndicator { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField emailField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton loginBtn { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField pwField { get; set; }
+
+        [Action ("LoginBtn_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void LoginBtn_TouchUpInside (UIKit.UIButton sender);
+
         void ReleaseDesignerOutlets ()
         {
+            if (activityIndicator != null) {
+                activityIndicator.Dispose ();
+                activityIndicator = null;
+            }
+
+            if (emailField != null) {
+                emailField.Dispose ();
+                emailField = null;
+            }
+
+            if (loginBtn != null) {
+                loginBtn.Dispose ();
+                loginBtn = null;
+            }
+
+            if (pwField != null) {
+                pwField.Dispose ();
+                pwField = null;
+            }
         }
     }
 }
